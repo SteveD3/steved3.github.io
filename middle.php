274407d933cabd1b85bf00064c05a0425678cfce
Steve Ragan <SteveD3@users.noreply.github.com>
2018-07-03 23:22:53 -0400

    Template

---
+++ middle.php
@@ -0,0 +1,12 @@
+<body>
+
+<div id="banner">
+
+</div>
+
+<div id="menu">
+<a href="index.php">Home</a> |
+<a href="sitemap.php">Sitemap</a> |
+<a href="products.php">Products</a> |
+<a href="contact.php">Contact Us</a>
+</div>
